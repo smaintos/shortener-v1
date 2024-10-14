9890fe68da8fb93a2de8fa32c14ab5ba509031f6
smaintos <w.mariamonmw@gmail.com>
2024-10-14 19:28:10 +0200

    add features

--- shortener/src/components/linkbar.tsx
+++ shortener/src/components/linkbar.tsx
@@ -19,8 +19,10 @@ const LinkBar: React.FC = () => {
   const [linkedInUrl, setLinkedInUrl] = useState('');
   const [mailtoUrl, setMailtoUrl] = useState('');
   const [error, setError] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
 
   const handleShortenLink = async () => {
+    setIsLoading(true);
     try {
       const response = await axios.post(
         'https://api.tinyurl.com/create',
@@ -30,7 +32,8 @@ const LinkBar: React.FC = () => {
         },
         {
           headers: {
-            Authorization: 'Bearer nmAk2hKgjD320LuhVhEs4LHPrD2UmzFiQfuAfB37kXrVp90g697X1Ej2pphh',
+            Authorization:
+              'Bearer nmAk2hKgjD320LuhVhEs4LHPrD2UmzFiQfuAfB37kXrVp90g697X1Ej2pphh',
             'Content-Type': 'application/json',
           },
         }
@@ -48,7 +51,9 @@ const LinkBar: React.FC = () => {
       setGmailComposeUrl(gmailUrl);
 
       // Générer l'URL de partage Twitter
-      const tweetText = encodeURIComponent(`Voici le lien raccourci : ${shortened}`);
+      const tweetText = encodeURIComponent(
+        `Voici le lien raccourci : ${shortened}`
+      );
       const twitterShareUrl = `https://twitter.com/intent/tweet?url=${encodeURIComponent(
         shortened
       )}&text=${tweetText}`;
@@ -61,7 +66,9 @@ const LinkBar: React.FC = () => {
       setFacebookUrl(fbUrl);
 
       // Générer l'URL de partage WhatsApp
-      const whatsappText = encodeURIComponent(`Voici le lien raccourci : ${shortened}`);
+      const whatsappText = encodeURIComponent(
+        `Voici le lien raccourci : ${shortened}`
+      );
       const whatsappShareUrl = `https://api.whatsapp.com/send?text=${whatsappText}`;
       setWhatsappUrl(whatsappShareUrl);
 
@@ -89,11 +96,13 @@ const LinkBar: React.FC = () => {
       setWhatsappUrl('');
       setLinkedInUrl('');
       setMailtoUrl('');
+    } finally {
+      setIsLoading(false);
     }
   };
 
   return (
-    <div className="max-w-md mx-auto mt-10 p-6 bg-slate-800 shadow-md rounded">
+    <div className="w-[500px] mx-auto mt-10 p-6 bg-slate-800 shadow-md rounded h-[500px] flex flex-col">
       <h2 className="text-2xl font-bold mb-4 text-center text-slate-100">
         Link Shortener v1
       </h2>
@@ -108,84 +117,96 @@ const LinkBar: React.FC = () => {
       </div>
       <button
         onClick={handleShortenLink}
-        className="w-full p-2 bg-blue-600 text-white font-semibold rounded hover:bg-blue-700"
+        className="w-full p-2 bg-blue-600 text-white font-semibold rounded hover:bg-blue-700 disabled:bg-blue-500"
+        disabled={isLoading}
       >
         Raccourcir le Lien
       </button>
       {error && <div className="mt-4 text-red-500">{error}</div>}
-      {shortenedLink && (
-        <div className="mt-6">
-          <h3 className="text-lg font-semibold text-slate-100">Lien Raccourci :</h3>
-          <a
-            href={shortenedLink}
-            target="_blank"
-            rel="noopener noreferrer"
-            className="text-blue-400 break-all"
-          >
-            {shortenedLink}
-          </a>
-          <div className="mt-4 flex flex-wrap gap-4">
-            {/* Bouton Gmail */}
-            <a
-              href={gmailComposeUrl}
-              target="_blank"
-              rel="noopener noreferrer"
-              className="text-red-500 hover:text-red-600 text-2xl"
-              aria-label="Envoyer par Gmail"
-            >
-              <FaGoogle />
-            </a>
-            {/* Bouton Twitter */}
-            <a
-              href={twitterUrl}
-              target="_blank"
-              rel="noopener noreferrer"
-              className="text-blue-400 hover:text-blue-500 text-2xl"
-              aria-label="Partager sur Twitter"
-            >
-              <FaTwitter />
-            </a>
-            {/* Bouton Facebook */}
-            <a
-              href={facebookUrl}
-              target="_blank"
-              rel="noopener noreferrer"
-              className="text-blue-600 hover:text-blue-700 text-2xl"
-              aria-label="Partager sur Facebook"
-            >
-              <FaFacebook />
-            </a>
-            {/* Bouton WhatsApp */}
-            <a
-              href={whatsappUrl}
-              target="_blank"
-              rel="noopener noreferrer"
-              className="text-green-500 hover:text-green-600 text-2xl"
-              aria-label="Partager sur WhatsApp"
-            >
-              <FaWhatsapp />
-            </a>
-            {/* Bouton LinkedIn */}
-            <a
-              href={linkedInUrl}
-              target="_blank"
-              rel="noopener noreferrer"
-              className="text-blue-700 hover:text-blue-800 text-2xl"
-              aria-label="Partager sur LinkedIn"
-            >
-              <FaLinkedin />
-            </a>
-            {/* Bouton Email */}
-            <a
-              href={mailtoUrl}
-              className="text-slate-100 hover:text-slate-200 text-2xl"
-              aria-label="Envoyer par Email"
-            >
-              <FaEnvelope />
-            </a>
-          </div>
-        </div>
-      )}
+
+      {/* Conteneur pour le contenu dynamique */}
+      <div className="mt-6 flex-1 flex items-center justify-center">
+        {isLoading ? (
+          // Spinner de chargement
+          <div className="animate-spin rounded-full h-16 w-16 border-t-4 border-b-4 border-blue-500"></div>
+        ) : (
+          shortenedLink && (
+            <div>
+              <h3 className="text-lg font-semibold text-slate-100 text-center">
+                Lien Raccourci :
+              </h3>
+              <a
+                href={shortenedLink}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-blue-400 break-all text-center block"
+              >
+                {shortenedLink}
+              </a>
+              <div className="mt-4 flex flex-wrap gap-4 justify-center">
+                {/* Bouton Gmail */}
+                <a
+                  href={gmailComposeUrl}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="text-red-500 hover:text-red-600 text-2xl"
+                  aria-label="Envoyer par Gmail"
+                >
+                  <FaGoogle />
+                </a>
+                {/* Bouton Twitter */}
+                <a
+                  href={twitterUrl}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="text-blue-400 hover:text-blue-500 text-2xl"
+                  aria-label="Partager sur Twitter"
+                >
+                  <FaTwitter />
+                </a>
+                {/* Bouton Facebook */}
+                <a
+                  href={facebookUrl}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="text-blue-600 hover:text-blue-700 text-2xl"
+                  aria-label="Partager sur Facebook"
+                >
+                  <FaFacebook />
+                </a>
+                {/* Bouton WhatsApp */}
+                <a
+                  href={whatsappUrl}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="text-green-500 hover:text-green-600 text-2xl"
+                  aria-label="Partager sur WhatsApp"
+                >
+                  <FaWhatsapp />
+                </a>
+                {/* Bouton LinkedIn */}
+                <a
+                  href={linkedInUrl}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="text-blue-700 hover:text-blue-800 text-2xl"
+                  aria-label="Partager sur LinkedIn"
+                >
+                  <FaLinkedin />
+                </a>
+                {/* Bouton Email */}
+                <a
+                  href={mailtoUrl}
+                  className="text-slate-100 hover:text-slate-200 text-2xl"
+                  aria-label="Envoyer par Email"
+                >
+                  <FaEnvelope />
+                </a>
+              </div>
+            </div>
+          )
+        )}
+      </div>
     </div>
   );
 };
